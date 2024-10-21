a287b6484620c2ce0da01bf158de701be8439b45
Hanjin99 <qq784365536@gmail.com>
2024-10-21 10:34:56 +0200

    Update Idee zur Aufgabe 4 - Xu.R

--- Projekt 1/Idee zur Aufgabe 4 - Xu.R
+++ Projekt 1/Idee zur Aufgabe 4 - Xu.R
@@ -48,19 +48,21 @@ life_expectancy_data <- cbind(avg_life_expectancy_2002, avg_life_expectancy_2022
 print(life_expectancy_data)
 
 # Grafikparameter anpassen, um Platz für die Legende rechts vom Plot zu schaffen
-par(mar = c(4,4,2,10))
+par(mar = c(4,4,2,10), xpd = TRUE)
 
 # Linienplot erstellen (x-Achse sind die Jahre, y-Achse ist die Lebenserwartung)
 matplot(years, t(life_expectancy_data), type = "b", pch = 19, lty = 1, 
         col = c("#619CFF", "#00BA38", "#F8766D", "#C77CFF", "#B79F00"),
         xlab = "Jahr", ylab = "Durchschnittliche Lebenserwartung",
-        main = "Veränderung der Lebenserwartung in den Regionen (2002 vs 2022)",
+        main = "Veränderung der gesamten Lebenserwartung in den Regionen (2002 vs. 2022)",
         xlim = c(2000, 2025), ylim = c(55, 80))
 
 # Legende außerhalb des Plots platzieren (rechts)
 legend("topright", inset = c(-0.46, 0), legend = regions, 
        col = c("#619CFF", "#00BA38", "#F8766D", "#C77CFF", "#B79F00"), 
        pch = 19, lty = 1, cex = 1, box.lty = 0)
+
+#Speichern mit 830 x 500
 
 #########
 
