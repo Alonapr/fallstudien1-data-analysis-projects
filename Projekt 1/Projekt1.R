92d5140e6c3bea1e2083d7ad177839aff6ab8a4b
Alona Predkova <alena.predkova21@gmail.com>
2024-10-23 13:58:36 +0200

    Update Projekt1.R

--- Projekt 1/Projekt1.R
+++ Projekt 1/Projekt1.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-#setwd("C:/Users/alena/Documents/GitHub/Fallstudien-1/Projekt 1")
+setwd("C:/Users/alena/Documents/GitHub/Fallstudien-1/Projekt 1")
 
 data <- read.csv("census_2022_2002.csv")
 # Ueberblick ueber die Daten
@@ -48,26 +48,30 @@ num_stat <- function(data) {
 num_data <- 3:6
 num_stat(data2022[,num_data])
 
-# Boxplot fuer Lebenserwartung (+ Unterschiede zwischen den Geschlechtern)
+# Boxplots der Lebenserwartung (+ Unterschiede zwischen den Geschlechtern)
+par(mar = c(5, 5, 1, 2))
 boxplot(Life_Expectancy_Overall,
         Life_Expectancy_Male,
         Life_Expectancy_Female,
         names = c("Gesamt", "Männlich", "Weiblich"),
-        main = "Boxplots der Lebenserwartung",
         ylab = "Lebenserwartung (in Jahren)",
         xlab = "Merkmale", horizontal = FALSE, col = "lightblue")
+axis(2, at = seq(0, 100, by = 5))
+means <- c(mean(Life_Expectancy_Overall),
+           mean(Life_Expectancy_Male),
+           mean(Life_Expectancy_Female))
+segments(x0 = 1:3 - 0.4, y0 = means, x1 = 1:3 + 0.4, y1 = means, col = "red", lwd = 2)
+legend("bottomright", legend = "mean", fill = "red", bty = "n")
 
-# Unterschiede zwischen Geschlechter
+# Differenz der Lebenserwartung (Frauen - Männer)
 Diff_Lebenserwartung <- Life_Expectancy_Female - Life_Expectancy_Male
 hist(Diff_Lebenserwartung, xlab = "Differenz der Lebenserwartung (in Jahren)",
-     ylab = "Relative Häufigkeit",
-     main = "Differenz der Lebenserwartung (Frauen - Männer)", probability = TRUE,
-     ylim = c(0, 0.30), breaks = -2:12)
+     ylab = "Relative Häufigkeit",probability = TRUE,
+     ylim = c(0, 0.30), breaks = -2:12, main = "")
 
-# Boxplot fuer gesamte Fertilitaet
-hist(Total_Fertility_Rate, main = "Histogramm für Fertilitätsrate",
-        xlab = "Fertilitätsrate", ylab = "Relative Häufigkeit", 
-     probability = TRUE)
+# Histogramm für Fertilitätsrate
+hist(Total_Fertility_Rate, xlab = "Fertilitätsrate", 
+     ylab = "Relative Häufigkeit", probability = TRUE, main = "")
 
 # Verteilung der Region mit Tabelle
 
@@ -83,19 +87,19 @@ table(Subregion)
 # Subregionen nach Region
 table(Subregion, Region)
 
-# Streudiagramm der Lebenserwartung vs. Fertilitätsrate
+# Zusammenhang zwischen gesamten Lebenserwartung und Fertilitätsrate
+# Streudiagramm
 ggplot(data2022, aes(x = Life_Expectancy_Overall, y = Total_Fertility_Rate)) +
   geom_point() +
-  labs(title = "Zusammenhang zwischen Lebenserwartung und Fertilitätsrate",
-       x = "Gesamte Lebenserwartung",
+  labs(x = "Gesamte Lebenserwartung",
        y = "Fertilitätsrate")
 
-# Streudiagramm der Lebenserwartung von Maenner vs Frauen
+# Zusammenhang zwischen Lebenserwartung von Frauen und Männer
+# Streudiagramm
 ggplot(data2022, aes(x = Life_Expectancy_Female, y = Life_Expectancy_Male)) +
   geom_point() +
   geom_abline(intercept = 0, slope = 1, color = "red", linetype = "solid") +
-  labs(title = "Zusammenhang zwischen Lebenserwartung von Frauen und Männer",
-       x = "Weibliche Lebenserwartung",
+  labs(x = "Weibliche Lebenserwartung",
        y = "Männliche Lebenserwartung") +
   theme(
     axis.title.x = element_text(size = 12),
@@ -103,18 +107,15 @@ ggplot(data2022, aes(x = Life_Expectancy_Female, y = Life_Expectancy_Male)) +
   )
 
 # Boxplot der Lebenserwartung nach Region
-
 region_name = c("Afrika", "Amerika", "Asien", "Europa", "Ozeanien")
 
 boxplot(Life_Expectancy_Overall ~ Region,
-        main = "Boxplot der Lebenserwartung nach Region",
         ylab = "Gesamte Lebenserwartung (in Jahren)",
         xlab = "Region", horizontal = FALSE,
         names = region_name, col = "lightblue")
 
-# Boxplot der Fertilitaet nach Region
-boxplot(Total_Fertility_Rate ~ Region,
-        main = "Boxplot der Fertilitätsrate nach Region",
+# Boxplot der Fertilitätsrate nach Region
+boxplot(Total_Fertility_Rate ~ Region, 
         ylab = "Fertilitätsrate",
         xlab = "Region", horizontal = FALSE,
         col = "lightblue", names = region_name)
@@ -160,7 +161,6 @@ year2022 <- data2022[order(Region, Subregion), ]
 #########################
 #Boxplots
 
-
 #Boxplot Lebenserwartung der Männer in den Subregionen
 # Boxplot mit ggplot2, nach Region gruppiert und mit farblicher Unterscheidung der Regionen
 ggplot(year2022, aes(x = factor(Subregion, levels = unique(Subregion)), y = Life_Expectancy_Male, fill = Region)) +
@@ -195,38 +195,38 @@ ggplot(year2022, aes(x = factor(Subregion, levels = unique(Subregion)), y = Life
     legend.title = element_text(size = 14)  # Legendentitel größer
   )
 
-# Boxplot Gesamte Lebenserwartung in den Subregionen
+# Gesamte Lebenserwartung in den Subregionen (2022)
 ggplot(year2022, aes(x = factor(Subregion, levels = unique(Subregion)), y = Life_Expectancy_Overall, fill = Region)) +
   geom_boxplot(coef = 1.5, size = 0.6) +
-  labs(title = "Gesamte Lebenserwartung in den Subregionen (2022)",
-       x = "Subregion",
-       y = "Lebenserwartung",
-       fill = "Region") +  # Legende für die Regionen hinzufügen
+  labs(x = "Subregion",
+       y = "Gesamte Lebenserwartung",
+       fill = "Region:") +  # Legende für die Regionen hinzufügen
   theme(
-    plot.title = element_text(size = 16, face = "bold"),  # Überschrift größer und fett
-    axis.title.x = element_text(size = 14),  # x-Achsenbeschriftung größer
-    axis.title.y = element_text(size = 14, margin = margin(r = 10)),  # y-Achsenbeschriftung größer mit Abstand
-    axis.text.x = element_text(angle = 45, hjust = 1), # x-Achsenbeschriftung um 45 Grad drehen
-    legend.text = element_text(size = 12),  # Legendentext größer
-    legend.title = element_text(size = 14)  # Legendentitel größer
-  )
+    plot.title = element_text(size = 16, face = "bold"),
+    axis.title.x = element_text(size = 14), 
+    axis.title.y = element_text(size = 14, margin = margin(r = 10)), 
+    axis.text.x = element_text(angle = 45, hjust = 1), 
+    legend.text = element_text(size = 12), 
+    legend.title = element_text(size = 14),
+    legend.position = "top",
+    legend.direction = "horizontal")
 
 
-# Boxplot Fertilitätsrate in den Subregionen
+# Fertilitätsrate in den Subregionen (2022)
 ggplot(year2022, aes(x = factor(Subregion, levels = unique(Subregion)), y = Total_Fertility_Rate, fill = Region)) +
   geom_boxplot(coef = 1.5, size = 0.6) +
-  labs(title = "Fertilitätsrate in den Subregionen (2022)",
-       x = "Subregion",
+  labs(x = "Subregion",
        y = "Fertilitätsrate",
-       fill = "Region") +  # Legende für die Regionen hinzufügen
+       fill = "Region:") +  # Legende für die Regionen hinzufügen
   theme(
-    plot.title = element_text(size = 16, face = "bold"),  # Überschrift größer und fett
-    axis.title.x = element_text(size = 14),  # x-Achsenbeschriftung größer
-    axis.title.y = element_text(size = 14, margin = margin(r = 10)),  # y-Achsenbeschriftung größer mit Abstand
-    axis.text.x = element_text(angle = 45, hjust = 1), # x-Achsenbeschriftung um 45 Grad drehen
-    legend.text = element_text(size = 12),  # Legendentext größer
-    legend.title = element_text(size = 14)  # Legendentitel größer
-  )
+    plot.title = element_text(size = 16, face = "bold"), 
+    axis.title.x = element_text(size = 14), 
+    axis.title.y = element_text(size = 14, margin = margin(r = 10)),  
+    axis.text.x = element_text(angle = 45, hjust = 1), 
+    legend.text = element_text(size = 12),  
+    legend.title = element_text(size = 14),
+    legend.position = "top",
+    legend.direction = "horizontal")
 
 #####################
 #Zusätzliche Grafik
@@ -247,8 +247,20 @@ ggplot(year2022, aes(x = factor(Subregion, levels = unique(Subregion)), y = Life
     legend.title = element_text(size = 14)  # Legendentitel größer
   )
 
+detach(data2022)
 
 #4 - Vergleich von Merkmalen in 2002 und 2022
 
-detach(data2022)
+par(mfrow = c(1, 2))
+# Gesamte Lebenserwartung in 2002 und 2022
+boxplot(data2002$Life_Expectancy_Overall, data2022$Life_Expectancy_Overall,
+        names = c("2002", "2022"),
+        ylab = "Gesamte Lebenserwartung (in Jahren)", 
+        xlab = "Jahr", col = "lightblue")
+
+# Fertilitätsrate in 2002 und 2022
+boxplot(data2002$Total_Fertility_Rate, data2022$Total_Fertility_Rate,
+        names = c("2002", "2022"), ylab = "Fertilitätsrate", 
+        xlab = "Jahr", col = "lightblue")
+
 
