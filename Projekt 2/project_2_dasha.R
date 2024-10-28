984aaf4b17fceb711a296874da12eea9700cc215
Daria Sizeva <92261857+dariasi@users.noreply.github.com>
2024-10-28 13:51:02 +0100

    Update project_2_dasha.R

--- Projekt 2/project_2_dasha.R
+++ Projekt 2/project_2_dasha.R
@@ -1,25 +1,34 @@
+library(car)
+
 df <- read.table("Konzentrationsdaten.txt", header=TRUE)
 head(df)
 
-# Extremwerte wegwerfen:
+# Vorbereitung von Daten
+
+# Entfernen der Zeile mit Extremwerten:
 df <- df[df$id != "14",]
+
+# Umwandlung der Variablen in Faktoren:
+df$gruppe <- as.factor(df$gruppe) 
+df$id <- as.factor(df$id)
+df$durchgang <- as.factor(df$durchgang)
+df$test_typ <- as.factor(df$test_typ)
+
 
 # Aufgabe 1
 
 df_durchgang1 <- df[df$durchgang == "1", ]
-df_durchgang1_gu <- df[df$durchgang == "1" & df$test_typ == "gu", ]
-df_durchgang1_ug <- df[df$durchgang == "1" & df$test_typ == "ug", ]
 
-boxplot(df_durchgang1_gu$KL,
-        df_durchgang1_ug$KL,
+boxplot(df_durchgang1[df_durchgang1$test_typ == "gu", ]$KL,
+        df_durchgang1[df_durchgang1$test_typ == "ug", ]$KL,
         names = c("gu", "ug"),
         ylab = "KL",
         xlab = "Aufgabe für den ersten Durchgang", horizontal = FALSE, col = "lightblue")
 
-hist(df_durchgang1_gu$KL, xlab = "KL für gu", 
+hist(df_durchgang1[df_durchgang1$test_typ == "gu", ]$KL, xlab = "KL für gu", 
      ylab = "Relative Häufigkeit", probability = TRUE, main = "")
 
-hist(df_durchgang1_ug$KL, xlab = "KL für ug", 
+hist(df_durchgang1[df_durchgang1$test_typ == "ug", ]$KL, xlab = "KL für ug", 
      ylab = "Relative Häufigkeit", probability = TRUE, main = "")
 
 # Rechne Kolmogorov-Smirnov-Test auf Normalverteilung
@@ -27,9 +36,9 @@ hist(df_durchgang1_ug$KL, xlab = "KL für ug",
 # Stichprobenvarianz als e-treuen Schätzer der Varianz
 
 mu <- mean(df_durchgang1$KL)
-# 10.17805
-stand_abw <- sd(df_durchgang1$KL)
-# 45.08926
+# 11.0475
+sigma2 <- var(df_durchgang1$KL)
+# 14.45692
 
 # ks.test aus base R als Test auf Normalverteilung
 ks.test(df_durchgang1$KL, y="pnorm", mean=mu, sd=sqrt(sigma2))
@@ -37,26 +46,33 @@ ks.test(df_durchgang1$KL, y="pnorm", mean=mu, sd=sqrt(sigma2))
 # Asymptotic one-sample Kolmogorov-Smirnov test
 # 
 # data:  df_durchgang1$KL
-# D = 0.21325, p-value = 0.04804
+# D = 0.08848, p-value = 0.9129
 # alternative hypothesis: two-sided
 
-# Wir verwerfen die H0, dass sie aus einer N(mu, sqrt(sigma2))-Verteilung stammen
-# -> die Daten sind nicht normalverteilt
+# Wir verwerfen die H0 nicht, dass sie aus einer N(mu, sqrt(sigma2))-Verteilung 
+# stammen -> nichts spricht dagegen, dass die Daten normalverteilt sind
 
-wilcox.test(df_durchgang1_gu$KL, df_durchgang1_ug$KL, alternative = "two.sided")
+# Zusätzlich können wir das QQ-Plot erstellen, um visuell die NV-Annahme zu überprüfen
+qqnorm(df_durchgang1$KL, main = "Q-Q Plot für KL")
+qqline(df_durchgang1$KL, col = "red", lwd = 2)
 
-# Wilcoxon rank sum test with continuity correction
+t.test(KL ~ gruppe, data = df_durchgang1)
+
+# Diese Funktion überprüft schon impizit, ob Varianzen gleich sind (Welch’s t-test)
+
+# Two Sample t-test
 # 
-# data:  df_durchgang1_gu$KL and df_durchgang1_ug$KL
-# W = 218, p-value = 0.8449
-# alternative hypothesis: true location shift is not equal to 0
+# data:  KL by gruppe
+# t = 0.34122, df = 38, p-value = 0.7348
+# alternative hypothesis: true difference in means between group 1 and group 2 is not equal to 0
+# 95 percent confidence interval:
+#   -2.047119  2.877119
+# sample estimates:
+#   mean in group 1 mean in group 2 
+# 11.255          10.840 
 
-# -> keine signifikante Unterschiede
+# p-value = 0.7348 => die Unterschiede sind nicht signifikant
 
-
-# Aufgabe 2
-df_ohne_gruppen <- df[, -c(df$gruppe)]
-head(df_ohne_gruppen)
 
 
 
