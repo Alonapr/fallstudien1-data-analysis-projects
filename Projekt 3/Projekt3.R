4bc4c775324eaa6aa1641ac9eab099b5d3553682
Alona Predkova <alena.predkova21@gmail.com>
2024-11-18 11:19:55 +0100

    Create Projekt3.R

---
+++ Projekt 3/Projekt3.R
@@ -0,0 +1,23 @@
+setwd("C:/Users/alena/D/Universität/5Semester/Fallstudien 1/3")
+df <- read.table("Kuckuckseier.txt", header=TRUE)
+head(df)
+dim(df)
+#45 4
+
+str(df)
+
+# Überprüfung auf NA-Werte und Anschauen der Verteilung
+summary(df)
+
+par(mar = c(4.2, 4, 1, 1), mfrow = c(2, 2))
+hist(WP, main = "", ylab = "Empirische Dichte", probability = TRUE, 
+     cex.axis = 1.1, cex.lab = 1.3)
+hist(BP, main = "", ylab = "Empirische Dichte", probability = TRUE, 
+     cex.axis = 1.1, cex.lab = 1.3)
+hist(RK, main = "", ylab = "Empirische Dichte", probability = TRUE, 
+     cex.axis = 1.1, cex.lab = 1.3)
+hist(ZK, main = "", ylab = "Empirische Dichte", probability = TRUE, 
+     cex.axis = 1.1, cex.lab = 1.3)
+
+
+
