f9a05dad001e77797edb420df371a283ee4e90ee
Alona Predkova <alena.predkova21@gmail.com>
2025-01-13 11:43:50 +0100

    Update Projekt5_Alona.R

--- Projekt 5/Projekt5_Alona.R
+++ Projekt 5/Projekt5_Alona.R
@@ -54,15 +54,11 @@ num_stat(df[,c(5, 6, 8:10)])
 barplot(table(Birth_Rate), main = "", ylab = "Anzahl", xlab = "Birth_Rate", 
         cex.axis = 1.1, cex.lab = 1.3) 
 
-qqnorm(Population, xlab = "Theoretisches Quantil", ylab = "Empirisches Quantil",
-       main = "GU Test")
-qqline(Population, col = "red", lwd = 2)
-
 #Aufgabe 1 - die Zielvariable Leading_Candidate modellieren
 
 # Harris = 0, Trump = 1 kodieren (da wir Logistische Regression verwenden, 
 # muss die Zielvariable numerisch sein und als 0 und 1 kodiert werden)
-Leading_Candidate <- ifelse(Leading_Candidate == "Harris", 0, 1)
+df$Leading_Candidate <- ifelse(df$Leading_Candidate == "Harris", 0, 1)
 
 glm_model <- glm(Leading_Candidate ~ Total_Area + Population + Population_Density + 
                     Median_Age + Birth_Rate + HDI + Unemployment_Rate + 
@@ -82,4 +78,36 @@ plot(roc_curve, col = "blue", main = "ROC Curve")
 
 # AUC berechnen und ausgeben
 auc(roc_curve)
+
+
+# TrainControl für Kreuzvalidierung mit ROC und AUC
+train_control <- trainControl(method = "cv",         
+                              number = 10,           # 10-fache Kreuzvalidierung
+                              classProbs = TRUE,     # Berechnung von Klassenwahrscheinlichkeiten
+                              summaryFunction = twoClassSummary)  # Berechnung von ROC und AUC
+
+# Das Modell trainieren (binomiale logistische Regression)
+cv_model <- train(Leading_Candidate ~ Total_Area + Population + Population_Density + 
+                    Median_Age + Birth_Rate + HDI + Unemployment_Rate + 
+                    Health_Insurance_Coverage + Median_Rent, 
+                  data = df, 
+                  method = "glm", 
+                  family = "binomial", 
+                  trControl = train_control, 
+                  metric = "ROC")  # ROC als Leistungsmaß
+
+# Zusammenfassung der Ergebnisse der Kreuzvalidierung
+print(cv_model)
+
+# ROC-Kurve berechnen und plotten
+pred_prob <- predict(cv_model, df, type = "prob")[,2]  # Wahrscheinlichkeiten für Klasse 1 (Trump)
+roc_curve <- roc(df$Leading_Candidate, pred_prob)
+
+# Plot der ROC-Kurve
+plot(roc_curve, col = "blue", main = "ROC Curve")
+
+# AUC berechnen und anzeigen
+auc_value <- auc(roc_curve)
+print(paste("AUC:", auc_value))
+
 
