354d0e390512156d8b0bcd1f8f3786b356500489
KatrinBerger <119357325+KatrinBerger@users.noreply.github.com>
2025-01-27 21:29:44 +0100

    Update Projekt5.R

--- Projekt 5/Projekt5.R
+++ Projekt 5/Projekt5.R
@@ -61,6 +61,7 @@ hist(df$Population_Density)
 # Harris = 0, Trump = 1 kodieren (da wir Logistische Regression verwenden, 
 # muss die Zielvariable numerisch sein und als 0 und 1 kodiert werden)
 df$Leading_Candidate <- ifelse(df$Leading_Candidate == "Harris", 0, 1)
+df$Leading_Candidate <- as.factor(df$Leading_Candidate)
 
 # Logistische Regression
 Modell_voll <- glm(df$Leading_Candidate ~ df$Total_Area + df$Population
